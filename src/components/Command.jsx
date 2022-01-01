c56840166fdd0cc53c3b9c9bfafc190220a43bac
CountBleck <Mr.YouKnowWhoIAm@protonmail.com>
2021-12-31 20:36:28 -0800

    Added ability to abort commands.

--- src/components/Command.jsx
+++ src/components/Command.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 export default class Command extends React.Component {
-    handleClick = async () => {
+    handleAbort = () => {
+        this.setState({disabled: true, running: true})
+    }
+    
+    handleExecute = async () => {
         const {command, guild, tasks, onTaskChange} = this.props
 
         const iterator = command.exec(guild)
@@ -11,21 +15,31 @@ export default class Command extends React.Component {
         }
 
         tasks.set(command.id, progress)
-        this.setState({disabled: true})
+        this.setState({disabled: false, running: true})
         onTaskChange()
 
+        let aborted = false
         for await (const [percent, message] of iterator) {
+            // Check if aborting
+            if (this.state.running && this.state.disabled) {
+                aborted = true
+                break
+            }
+
             progress.percent = isNaN(percent) ? 1 : Math.min(Math.max(percent, 0), 1)
             if (message) progress.message = message
             onTaskChange()
         }
 
         progress.percent = 1
-        progress.message = "Done!"
+        progress.message = aborted ? "Aborted" : "Done!"
+
+        this.setState({disabled: true, running: true})
+        onTaskChange()
 
         setTimeout(() => {
             tasks.delete(command.id)
-            this.setState({disabled: false})
+            this.setState({disabled: false, running: false})
             onTaskChange()
         }, 500)
     }
@@ -33,21 +47,27 @@ export default class Command extends React.Component {
     constructor(props) {
         super(props)
 
-        const disabled = this.props.tasks.has(this.props.command.id)
-        this.state = {disabled}
+        const running = this.props.tasks.has(this.props.command.id)
+        this.state = {
+            running,
+            disabled: false
+        }
     }
 
     render() {
         const {command} = this.props
+        const {disabled} = this.state
+
+        const button = this.state.running
+            ? <button disabled={disabled} className="abort" onClick={this.handleAbort}>abort</button>
+            : <button disabled={disabled} onClick={this.handleExecute}>execute</button>
 
         return <div className="command">
             <div>
                 <h4>{command.id}</h4>
                 <p>{command.description}</p>
             </div>
-            <button disabled={this.state.disabled} onClick={this.handleClick}>
-                execute
-            </button>
+            {button}
         </div>
     }
 }
